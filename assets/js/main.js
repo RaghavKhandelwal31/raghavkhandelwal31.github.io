1cd5618599e5756a9e06210d5e666cf21909d716
RaghavKhandelwal31 <58084165+RaghavKhandelwal31@users.noreply.github.com>
2026-01-24 08:48:29 -0700

    feat: upgrade design with premium aesthetics, fluid typography, and fix animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,24 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-   if ($('.ds-testimonials-section').length) {
+	if ($('.ds-testimonials-section').length) {
 		$('.ds-testimonials-slider').slick({
-		  	infinite: true,
-		  	arrows: true,
-		  	autoplay: true,
-		  	autoplaySpeed: 4000,
-		  	prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='ri-arrow-left-line'></i></button>",
-		  	nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='ri-arrow-right-line'></i></button>"
+			infinite: true,
+			arrows: true,
+			autoplay: true,
+			autoplaySpeed: 4000,
+			prevArrow: "<button type='button' class='slick-prev slick-arrow'><i class='ri-arrow-left-line'></i></button>",
+			nextArrow: "<button type='button' class='slick-next slick-arrow'><i class='ri-arrow-right-line'></i></button>"
 		});
-    }
-   
+	}
+
+
+
+	// Initialize Animate On Scroll
+	AOS.init({
+		duration: 800,
+		easing: 'ease-in-out',
+		once: true,
+		mirror: false
+	});
 
 });
